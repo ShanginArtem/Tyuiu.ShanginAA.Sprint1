99f1bd06d45b8a04422fe8f80e81bb678cdf0cda
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-11-03 01:32:57 +0500

    Add project files.

---
+++ Tyuiu.ShanginAA.Sprint1.Task0.V1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.ShanginAA.Sprint1.Task0.V1.Lib;
+
+namespace Tyuiu.ShanginAA.Sprint1.Task0.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Шангин А. А. | ИСП6-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Шангин Артём Александрович | ИСПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* 36 / 2 / 9 + 1 - 6 / 2 * 3 и печатает результат на экране               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 36 / 2 / 9 + 1 - 6 / 2 * 3                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+
+        }
+    }
+}
