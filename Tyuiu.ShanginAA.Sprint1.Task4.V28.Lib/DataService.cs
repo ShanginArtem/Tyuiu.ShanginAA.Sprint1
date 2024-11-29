03100f191a957fa006ba9e58ecd7e05e8fce68f1
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-11-30 03:51:22 +0500

    task4V28

---
+++ Tyuiu.ShanginAA.Sprint1.Task4.V28.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ShanginAA.Sprint1.Task4.V28.Lib
+{
+    public class DataService : ISprint1Task4V28
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = (Math.Cos(60 * Math.PI / 2)) / (Math.Exp(2 * x + y));
+
+            double result = Math.Round(res, 3);
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.ShanginAA.Sprint1.Task4.V28/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ShanginAA.Sprint1.Task4.V28.Lib;
+
+namespace Tyuiu.ShanginAA.Sprint1.Task4.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Шангин А. А. | ИСП6-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Шангин Артём Александрович | ИСПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране. Ответ округлите*");
+            Console.WriteLine("* Формула: cos(60*pi / 2) / Exp(2 * x + y)                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double x;
+            double y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("cos(60*pi / 2) / Exp(2 * x + y) = " + ds.Calculate(x,y));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
