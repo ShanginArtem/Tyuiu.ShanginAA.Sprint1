6fa28a7aaf49994657e4d4bb185f4817db283315
Tyuiu.ShanginAA@outlook.com <Tyuiu.ShanginAA@outlook.com>
2024-12-02 02:54:06 +0500

    task5v3

---
+++ Tyuiu.ShanginAA.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.ShanginAA.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.ShanginAA.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int x = 54321;
+            DataService ds = new DataService();
+            int res = ds.Calculate(x);
+
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
